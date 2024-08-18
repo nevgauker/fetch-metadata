b302a5df7e28d7e05a894b6c9a31d70502c7c483
rotem <nevgauker@gmail.com>
2024-08-18 21:23:57 +0100

    first commit

---
+++ dist/server.js
@@ -0,0 +1,66 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
+const cheerio_1 = __importDefault(require("cheerio"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const meta_fetcher_1 = __importDefault(require("meta-fetcher"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+const limiter = (0, express_rate_limit_1.default)({
+    windowMs: 1000, // 1 second
+    max: 5, // Limit each IP to 5 requests per windowMs
+    message: { error: 'Too many requests, please try again later.' },
+});
+app.use(express_1.default.json());
+app.use(limiter);
+app.get('/test', () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const result = yield (0, meta_fetcher_1.default)('https://www.bbc.com/');
+        return (result);
+    }
+    catch (error) {
+        return (error);
+    }
+}));
+app.post('/fetch-metadata', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { urls } = req.body;
+    if (!urls || !Array.isArray(urls)) {
+        return res.status(400).json({ error: 'Please provide an array of URLs' });
+    }
+    const metadataPromises = urls.map((url) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const { data } = yield axios_1.default.get(url);
+            const $ = cheerio_1.default.load(data);
+            const title = $('head > title').text();
+            const description = $('meta[name="description"]').attr('content') || '';
+            const image = $('meta[property="og:image"]').attr('content') || '';
+            return {
+                url,
+                title,
+                description,
+                image,
+            };
+        }
+        catch (error) {
+            return { url, error: 'Failed to fetch metadata' };
+        }
+    }));
+    const metadata = yield Promise.all(metadataPromises);
+    res.json(metadata);
+}));
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ server.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from 'express';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import rateLimit from 'express-rate-limit';
+import metaFetcher from 'meta-fetcher';
+
+const app = express();
+const PORT = 3000;
+
+interface Metadata {
+    url: string;
+    title?: string;
+    description?: string;
+    image?: string;
+    error?: string;
+}
+
+const limiter = rateLimit({
+    windowMs: 1000, // 1 second
+    max: 5, // Limit each IP to 5 requests per windowMs
+    message: { error: 'Too many requests, please try again later.' },
+});
+
+app.use(express.json());
+app.use(limiter);
+
+
+app.get('/test', async () => {
+
+    try {
+        const result = await metaFetcher('https://www.bbc.com/');
+        return(result);
+    } catch(error){
+        return(error);
+    }
+
+});
+
+app.post('/fetch-metadata', async (req: Request, res: Response) => {
+    const { urls }: { urls: string[] } = req.body;
+
+    if (!urls || !Array.isArray(urls)) {
+        return res.status(400).json({ error: 'Please provide an array of URLs' });
+    }
+
+    const metadataPromises: Promise<Metadata>[] = urls.map(async (url) => {
+        try {
+            const { data } = await axios.get(url);
+            const $ = cheerio.load(data);
+
+            const title = $('head > title').text();
+            const description = $('meta[name="description"]').attr('content') || '';
+            const image = $('meta[property="og:image"]').attr('content') || '';
+
+            return {
+                url,
+                title,
+                description,
+                image,
+            };
+        } catch (error) {
+            return { url, error: 'Failed to fetch metadata' };
+        }
+    });
+
+    const metadata = await Promise.all(metadataPromises);
+    res.json(metadata);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
